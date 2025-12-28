d8d4742c70c5df0d5a472592ec1f09145a8e51d6
hide card <hidecard1500@gmail.com>
2025-12-28 09:45:33 +0630

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,93 @@
 import 'package:flutter/material.dart';
-
+import './screens/backup_restore_screen.dart';
+import './screens/cart_screen.dart';
+import './screens/checkout_screen.dart';
+import './screens/customer_screen.dart';
+import './screens/home_screen.dart';
+import './screens/order_history_screen.dart';
+import './screens/product_screen.dart';
+import 'package:get/get.dart';
+import 'package:badges/badges.dart';
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application. 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+Get.put(ProductController());
+Get.put(CartController());
+Get.put(CustomerController());
+Get.put(OrderController());
+return GetMaterialApp(
+title: 'POS System',
+theme: ThemeData(
+primarySwatch: Colors.indigo,
+elevatedButtonTheme: ElevatedButtonThemeData(
+style: ElevatedButton.styleFrom(
+padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+textStyle: TextStyle(fontSize: 16),
+),
+),
+),
+home: MainScreen(),
+getPages: [
+GetPage(name: '/cart', page: () => CartScreen()),
+GetPage(name: '/product_management', page: () =>
+ProductScreen()),
+GetPage(name: '/customer_management', page: () =>
+CustomerScreen()),
+GetPage(name: '/order_history', page: () => OrderHistoryScreen()),
+],
+);
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+class MainScreen extends StatelessWidget {
+final List<Widget> _screens = [
+HomeScreen(),
+ProductScreen(),
+CustomerScreen(),
+OrderHistoryScreen(),
+BackupRestoreScreen(), // New screen
+final RxInt _currentIndex = 0.obs;
+final CartController cartController = Get.find();
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+body: Obx(() => IndexedStack( // Use IndexedStack to preserve state across
+tabs
+index: _currentIndex.value,
+children: _screens,
+)),
+bottomNavigationBar: Obx(() => BottomNavigationBar(
+currentIndex: _currentIndex.value,
+onTap: (index) => _currentIndex.value = index,
+type: BottomNavigationBarType.fixed,
+selectedItemColor: Theme.of(context).primaryColor,
+unselectedItemColor: Colors.grey,
+items: [
+BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+BottomNavigationBarItem(icon: Icon(Icons.inventory), label:
+'Products'),
+BottomNavigationBarItem(icon: Icon(Icons.person), label:
+'Customers'),
+BottomNavigationBarItem(icon: Icon(Icons.history), label: 'Orders'),
+BottomNavigationBarItem(icon: Icon(Icons.backup), label: 'Backup'),
+// New tab
+],
+)),
+floatingActionButton: Obx(() => badges.Badge(
+badgeContent: Text(
+cartController.cartItems.length.toString(),
+style: TextStyle(color: Colors.white),
+),
+showBadge: cartController.cartItems.isNotEmpty,
+child: FloatingActionButton(
+heroTag: 'cart_fab',
+onPressed: () => Get.toNamed('/cart'),
+child: Icon(Icons.shopping_cart),
+),
+)),
+);
+}
 }
---
+++ lib/screens/backup_restore_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BackupRestoreScreen extends StatefulWidget {
+  const BackupRestoreScreen({super.key});
+
+  @override
+  State<BackupRestoreScreen> createState() => _BackupRestoreScreenState();
+}
+
+class _BackupRestoreScreenState extends State<BackupRestoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
